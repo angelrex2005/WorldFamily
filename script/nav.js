dd77651c29bf63300a1f1618a0f1c35fe0f58648
angelrex2005 <124093135+angelrex2005@users.noreply.github.com>
2023-02-02 11:21:55 -0500

    religioso

--- script/nav.js
+++ script/nav.js
@@ -6,5 +6,13 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-
+function copiarAlPortapapeles(id_elemento) {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+  alert("la ip del servidor se copio a su portapapeles")
+}
    
